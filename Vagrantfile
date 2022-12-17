8d89a60a527c134dd029a6c679ee79e0a026f567
Bruno Olmos Osorio <bruno.osorio@outlook.com>
2022-12-17 14:24:35 +0100

    p3 finished

--- Vagrantfile
+++ Vagrantfile
@@ -13,9 +13,19 @@ Vagrant.configure("2") do |config|
   end
 
   config.vm.provision "shell", inline: <<-SHELL
-	wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+	# vagrant, virtualbox, xfce, firefox, git, vim
+ 	wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 	wget https://download.virtualbox.org/virtualbox/7.0.4/virtualbox-7.0_7.0.4-154605~Debian~bullseye_amd64.deb
-	apt update && apt install -y git vim xfce4 vagrant linux-headers-5.10.0-16-amd64 ./virtualbox-7.0_7.0.4-154605~Debian~bullseye_amd64.deb
-  SHELL
+	apt-get update && apt-get install -y git vim xfce4 firefox-esr vagrant linux-headers-5.10.0-16-amd64 ./virtualbox-7.0_7.0.4-154605~Debian~bullseye_amd64.deb
+	# docker
+	curl -fsSL https://get.docker.com -o get-docker.sh
+	sh get-docker.sh
+	usermod -aG docker $USER
+	# kubectl
+	curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+	install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+	# k3d
+	wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+	SHELL
 end
--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 
-# install Mozilla (default browser does not work)
-sudo apt install -y firefox-esr
-
-# install Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-# install kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-# install k3d
-wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
-
 # create cluster
-sudo k3d cluster create mycluster -p 8080:80@loadbalancer -p 8888:8888@loadbalancer
-sudo kubectl create namespace argocd
-sudo kubectl create namespace dev
+k3d cluster create iot -p 8080:443@loadbalancer -p 8888:8888@loadbalancer
+kubectl create namespace argocd
+kubectl create namespace dev
 
 # deploy argocd
-sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-#need for argocd
-sudo kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-
-#playground
-sudo kubectl apply -f ../confs/app.yml -n dev
-
-#argocd password
-sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-
-#argocd login - admin
+# install argocd cli
+curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+rm argocd-linux-amd64
 
 
+# login. new password: inception
+while [ $(curl -s -o /dev/null localhost:8080; echo $?) -gt 0 ]; do
+	echo "Waiting for argocd..."
+	sleep 5
+done
+argocd login localhost:8080 --insecure --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
+argocd account update-password --current-password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) --new-password inception
+
+# deploy app
+kubectl config set-context --current --namespace=argocd
+argocd app create app --repo https://github.com/bolmos-o/bolmos-o --path iot --dest-server https://kubernetes.default.svc --dest-namespace dev
+argocd app sync app
+
+argocd app set app --sync-policy automated

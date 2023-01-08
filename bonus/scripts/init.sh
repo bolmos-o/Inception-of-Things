a4130229b7006e7ff256c5f15ccd465019365591
Bruno OLMOS OSORIO <bolmos-o@e1r11p7.clusters.42paris.fr>
2023-01-08 23:35:58 +0100

    Trying configs for gitlab

--- bonus/scripts/init.sh
+++ bonus/scripts/init.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # create cluster
-k3d cluster create iot -p 8080:443@loadbalancer -p 8888:8888@loadbalancer --k3s-arg="--disable=traefik@server:0"
+k3d cluster create iot -p 8080:8080@loadbalancer #-p 8888:8888@loadbalancer 
 kubectl create namespace argocd
 kubectl create namespace dev
-kubectl create namespace gitlab
-
-#installing gitlab with helm
-helm repo add gitlab https://charts.gitlab.io/
-helm repo update
-helm upgrade --install gitlab gitlab/gitlab --namespace gitlab \
-  --timeout 600s \
-  --set global.hosts.domain=iot \
-  --set certmanager-issuer.email=me@example.com \
-  --set postgresql.image.tag=13.6.0
-
-# set up hosts and get gitlab password
-sudo sh -c "echo `kubectl get svc -n gitlab | grep LoadBalancer | awk '{print $4}'` gitlab.iot >> /etc/hosts"
-
-# have to wait, otherwise the service is not up
-kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
 
 # # deploy argocd
 # kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 # kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
+#installing gitlab with helm
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+helm upgrade --install gitlab gitlab/gitlab \
+  --create-namespace --namespace gitlab \
+  --timeout 600s \
+  --set global.hosts.domain=iot \
+  --set nginx-ingress.enabled=false \
+  --set global.ingress.class=traefik \
+  --set global.ingress.provider=traefik\
+  --set certmanager-issuer.email=me@example.com \
+  --set postgresql.image.tag=13.6.0
+
 # # install argocd cli
 # curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 # sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 # rm argocd-linux-amd64
+
+# #port forward argocd
+# kubectl port-forward svc/argocd-server -n argocd 8080:443 &
 
 # # login. new password: inception
 # while [ $(curl -s -o /dev/null localhost:8080; echo $?) -gt 0 ]; do
@@ -38,6 +38,11 @@ kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.passwo
 # argocd login localhost:8080 --insecure --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
 # argocd account update-password --current-password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) --new-password inception
 
+# # set up hosts and get gitlab password
+# sudo sh -c "echo `kubectl get svc -n gitlab | grep LoadBalancer | awk '{print $4}'` gitlab.iot >> /etc/hosts"
+
+# # have to wait, otherwise the service is not up
+# kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
 
 # # deploy app
 # kubectl config set-context --current --namespace=argocd

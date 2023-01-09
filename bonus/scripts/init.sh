ec4864a39eb9a83ba8dfdaca36703fa97d42583f
Bruno OLMOS OSORIO <bolmos-o@e1r8p5.clusters.42paris.fr>
2023-01-09 23:50:54 +0100

    Changing k wait

--- bonus/scripts/init.sh
+++ bonus/scripts/init.sh
@@ -13,7 +13,8 @@ helm upgrade --install gitlab gitlab/gitlab \
   --timeout 600s \
   --set global.hosts.domain=example.com \
   --set certmanager-issuer.email=me@example.com \
-  --set postgresql.image.tag=13.6.0
+  --set postgresql.image.tag=13.6.0 \
+  --set global.edition=ce
 
 # deploy argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
@@ -23,14 +24,18 @@ curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/lat
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
 
+echo "Waiting for argocd server"
 # port-foward argocd
-kubectl wait --for=condition=Running pod/$(kubectl get pods -n argocd | grep argocd-server | awk '{print $1}')
+kubectl get all -n argocd
+kubectl wait --for=condition=Ready pod/$(kubectl get pods -n argocd | grep argocd-server | awk '{print $1}') --timeout=60s -n argocd
 kubectl port-forward svc/argocd-server -n argocd 8080:443 >> ~/argocd-log.txt 2>&1 &
 
+echo "Logging and setting argocd password."
 # change argocd password
 argocd login localhost:8080 --insecure --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
 argocd account update-password --current-password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo) --new-password inception
 
+echo "Getting gitlab password:"
 # get gitlab secret (might have to wait, need testing)
 kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
 
@@ -38,8 +43,8 @@ kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -ojsonpath='{.d
 # reference: https://docs.gitlab.com/charts/backup-restore/
 
 # # deploy app
-# kubectl config set-context --current --namespace=argocd
-# argocd app create app --repo gitlab.iot --path iot --dest-server https://kubernetes.default.svc --dest-namespace dev
+kubectl config set-context --current --namespace=argocd
+argocd app create app --repo gitlab.iot --path iot --dest-server https://kubernetes.default.svc --dest-namespace dev
 # argocd app sync app
 
 # argocd app set app --sync-policy automated

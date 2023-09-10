d4d028c31f56a3311dbd5f45495e378690bde2b0
Anton Myroshnychenko <antonmyroshnychenko@gmail.com>
2023-09-10 10:09:31 +0200

    apt install update

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 #linux-headers
-apt update && apt install -y curl vagrant virtualbox-7.0
+apt update && apt install -y curl vagrant virtualbox
 # docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh

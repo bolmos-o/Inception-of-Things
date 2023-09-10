73513d88ae36efc0cc70dba0277860b6773b01a6
bolmos-o <40002285+bolmos-o@users.noreply.github.com>
2023-09-10 13:15:01 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,15 +1,14 @@
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-#linux-headers
 apt update && apt install -y curl vagrant virtualbox
 # docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
-usermod -aG docker vagrant
+usermod -aG docker $USER
 # kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-cat >> /home/vagrant/.bashrc << EOF
+cat >> /home/$USER/.bashrc << EOF
 source <(kubectl completion bash)
 alias k=kubectl
 complete -o default -F __start_kubectl k
